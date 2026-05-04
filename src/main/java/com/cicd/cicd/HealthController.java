a704cef1f506bd1df44dcb24a64c573c03ec9bf4
makhsodur.rahman <makhsodur.rahman@bedatasolutions.com>
2026-05-05 01:20:03 +0600

    Commit test

--- src/main/java/com/cicd/cicd/HealthController.java
+++ src/main/java/com/cicd/cicd/HealthController.java
@@ -10,6 +10,6 @@ public class HealthController {
 
     @GetMapping("/health")
     public Map<String, String> health() {
-        return Map.of("status", "UP and running");
+        return Map.of("status", "UP and running check");
     }
 }

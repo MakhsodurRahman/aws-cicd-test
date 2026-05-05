7d0e7418621fc8706c1f224484e12c4951bac864
makhsodur.rahman <makhsodur.rahman@bedatasolutions.com>
2026-05-06 02:06:05 +0600

    Insha-allah

--- src/main/java/com/cicd/cicd/HealthController.java
+++ src/main/java/com/cicd/cicd/HealthController.java
@@ -10,6 +10,6 @@ public class HealthController {
 
     @GetMapping("/health")
     public Map<String, String> health() {
-        return Map.of("status", "UP and running check keno jani hobe na bb");
+        return Map.of("status", "UP and running check keno jani hobe na bb insha-allah ebar hobe");
     }
 }
